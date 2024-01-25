3b716f2197805a7ebfef7496289fdb4fc2fb4e27
Herland Mojica <mojicacardozoh@gmail.com>
2024-01-25 11:27:42 -0400

    actualizando el proyecto biblio

--- src/biblioteca_proyecto/Biblioteca.java
+++ src/biblioteca_proyecto/Biblioteca.java
@@ -6,4 +6,30 @@ public class Biblioteca {
     public List<Libro> librosDisponibles;
     public List<Libro> librosPrestados;
 
+    //constructores
+    public Biblioteca() {
+    }
+    public Biblioteca(List<Libro> librosDisponibles, List<Libro> librosPrestados) {
+        this.librosDisponibles = librosDisponibles;
+        this.librosPrestados = librosPrestados;
+    }
+    // getters y setters
+    public List<Libro> getLibrosDisponibles() {
+        return librosDisponibles;
+    }
+
+    public void setLibrosDisponibles(List<Libro> librosDisponibles) {
+        this.librosDisponibles = librosDisponibles;
+    }
+
+    public List<Libro> getLibrosPrestados() {
+        return librosPrestados;
+    }
+
+    public void setLibrosPrestados(List<Libro> librosPrestados) {
+        this.librosPrestados = librosPrestados;
+    }
+    //Metodo para prestar un libro. asegurando de actualizar la lista de los libros disponibles
+    // y prestados segun sea necesario
+
 }
